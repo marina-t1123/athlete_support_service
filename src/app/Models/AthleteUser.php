377d756df2fd5f46a685825612f88572396deea1
Marina <75340313+marina-t1123@users.noreply.github.com>
2022-04-07 17:19:02 +0900

    AthleteUserモデルの内容の変更。

--- src/app/Models/AthleteUser.php
+++ src/app/Models/AthleteUser.php
@@ -8,4 +8,40 @@ use Illuminate\Database\Eloquent\Model;
 class AthleteUser extends Model
 {
     use HasFactory;
+
+    //athlete_usersテーブルをこのモデルで使用できるように指定する
+    protected $table = 'athlete_users';
+
+    //fillableを使い、DBでの操作で値を変更したいカラムを指定する。
+    protected $fillable = ['name', 'affiliation', 'event', 'sex', 'age', 'tall', 'weight'];
+
+    //リレーションをはる
+    /**
+     * 登録選手の既往歴を取得する
+    */
+    public function medicalHistories()
+    {
+        return $this->hasMany();
+    }
+    /**
+     * 登録選手の既往歴を取得する
+    */
+    public function medicalQuestionnaires()
+    {
+        return $this->hasMany();
+    }
+    /**
+     * 登録選手のカルテを取得する
+    */
+    public function medicalRecords()
+    {
+        return $this->hasMany();
+    }
+    /**
+     * 登録選手のトレーニングメニューを取得する
+     */
+    public function workoutPlans()
+    {
+        return $this->hasMany();
+    }
 }
